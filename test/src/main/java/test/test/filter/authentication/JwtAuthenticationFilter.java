f4135af2cdf51f674309162f095115c9032682c6
Ji Seungmin <cswcsm02@gmail.com>
2022-12-13 19:45:28 +0900

    FEAT: 카카오 JWT 토큰 발급 및 검증 기능 구현

--- test/src/main/java/test/test/filter/authentication/JwtAuthenticationFilter.java
+++ test/src/main/java/test/test/filter/authentication/JwtAuthenticationFilter.java
@@ -6,6 +6,8 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.googleapis.util.Utils;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.jwk.JWK;
 import lombok.SneakyThrows;
@@ -24,7 +26,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
 
@@ -79,6 +85,42 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
             } else {
                 throw new IllegalArgumentException("Invalid ID token.");
             }
+        } else if (Case == KAKAO) {
+
+            String reqURL = "https://kapi.kakao.com/v2/user/me";
+
+            String accessToken = request.getHeader("Authorization");
+            System.out.println("accessToken = " + accessToken);
+
+            try {
+                URL url = new URL(reqURL);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+                conn.setRequestMethod("POST");
+                conn.setDoOutput(true);
+                conn.setRequestProperty("Authorization", "Bearer " + accessToken);
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String line = "";
+                String result = "";
+
+                while ((line = br.readLine()) != null) {
+                    result += line;
+                }
+
+                System.out.println("response body : " + result);
+
+                JsonElement element = JsonParser.parseString(result);
+
+                String id = element.getAsJsonObject().get("kakao_account").getAsJsonObject().get("email").getAsString();
+
+                email = id;
+                password = "117069320589700689011";
+
+                br.close();
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
         } else if (Case == BASIC) {
             ObjectMapper objectMapper = new ObjectMapper();
             LoginDto loginDto = null;
@@ -123,7 +165,6 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
         String jwtToken;
         try {
-
             //securitySigner를 통해 jwk 토큰을 받아옴
             jwtToken = securitySigner.getJwtToken(user, jwk);
 
